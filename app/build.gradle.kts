5719c237eacb602bd40e537d4fe573e1ce8dafa0
lucas <ramosmanhaes@outlook.com>
2024-04-08 16:01:45 -0300

    Adicionando corotina para streaming de video

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //ExoPlayer
+    implementation("androidx.media3:media3-exoplayer:1.3.0")
+    implementation("androidx.media3:media3-exoplayer-dash:1.3.0")
+    implementation("androidx.media3:media3-ui:1.3.0")
 }
--- app/src/main/java/com/lucas/videoplayer/MainActivity.kt
+++ app/src/main/java/com/lucas/videoplayer/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.lucas.videoplayer.screens.VideoPlayer
 import com.lucas.videoplayer.ui.theme.VideoPlayerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    VideoPlayer()
                 }
             }
         }
